ea45a0325f5946e447a2e4ce802bcb6926bbb0f5
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-03-04 18:07:17 +0200

    first review

--- public/admin/pages/users/create.php
+++ public/admin/pages/users/create.php
@@ -81,7 +81,6 @@ include $_SERVER['DOCUMENT_ROOT'] . "/functions/function.php";
                 </div>
                 <div class="form-group mb-0">
                   <div class="custom-control custom-checkbox">
-                  <label class="custom-control-label" for="is_admin">هل أنت مشرف</label>
                     <input type="checkbox" name="is_admin" class="custom-control-input" id="is_admin" value="1">
                     <label class="custom-control-label" for="is_admin">هل انت المشرف</label>
                   </div>
